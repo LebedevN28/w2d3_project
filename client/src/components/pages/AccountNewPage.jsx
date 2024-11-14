ac505a550781c1484c55a0a0dd18f8a8f4846414
EvgenyShchelchkov <galaxyaseria007@mail.ru>
2024-11-14 11:17:27 +0300

    rega front

--- client/src/components/pages/AccountNewPage.jsx
+++ client/src/components/pages/AccountNewPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -10,9 +9,26 @@ export default function AccountNewPage({ signUpHandler }) {
       <Col md={{ span: 6, offset: 3 }} className="mt-5">
         <h3 className="text-center">Регистрация</h3>
         <Form onSubmit={signUpHandler}>
-          <Form.Group className="mb-3" controlId="name">
+          <Form.Group className="mb-3" controlId="firstName">
             <Form.Label>Имя</Form.Label>
-            <Form.Control name="name" type="text" placeholder="Введите имя" />
+            <Form.Control name="firstName" type="text" placeholder="Введите имя" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="lastName">
+            <Form.Label>Фамилия</Form.Label>
+            <Form.Control name="lastName" type="text" placeholder="Введите фамилия" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="fatherName">
+            <Form.Label>Отчество</Form.Label>
+            <Form.Control name="fatherName" type="text" placeholder="Введите Отчество" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="registration">
+            <Form.Label>Регистрация</Form.Label>
+            <Form.Select aria-label="Default select example" name="registration" type="text">
+      <option>Выберете регистрацию</option>
+      <option value="1">Федеральный</option>
+      <option value="2">Муниципальный</option>
+      <option value="3">Региональный</option>
+    </Form.Select>
           </Form.Group>
           <Form.Group className="mb-3" controlId="email">
             <Form.Label>Email</Form.Label>
