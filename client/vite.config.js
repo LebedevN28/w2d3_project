5578b98df78d32a78618c0f137be85a53ca7adf0
LebedevN28@yandex.ru <lebedevn28@yandex.ru>
2024-11-15 17:53:35 +0300

    dsadsadsa

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../server/dist',
+  },
+  base: '/',
   server: {
     proxy: {
       '/api': {
